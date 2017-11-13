f314f5d07ede65ab66cacbd165a6db46cc00f40d
Andrew05 <alawre28@uwo.ca>
2017-11-12 20:29:55 -0500

    added storyboard for tasks and chat list

--- CareProviderApp/ChatViewController.swift
+++ CareProviderApp/ChatViewController.swift
@@ -29,7 +29,63 @@ class ChatViewController: JSQMessagesViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
+        return messages[indexPath.item] as! JSQMessageData
+    }
+    
+//    override func collectionView(collectionView: JSQMessagesCollectionView!, messageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageData! {
+//        return messages[indexPath.item]
+//    }
+    
+//    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAt indexPath: IndexPath!) -> JSQMessageBubbleImageDataSource! {
+//
+//
+//    }
+//
+  
+    
+        
+//    override func collectionView(collectionView: JSQMessagesCollectionView!, bubbleImageViewForItemAtIndexPath indexPath: NSIndexPath!) -> UIImageView! {
+//        let message = messages[indexPath.item]
+//
+//        if message.sender() == sender {
+//            return UIImageView(image: outgoingBubbleImageView.image, highlightedImage: outgoingBubbleImageView.highlightedImage)
+//        }
+//
+//        return UIImageView(image: incomingBubbleImageView.image, highlightedImage: incomingBubbleImageView.highlightedImage)
+//    }
+    
+//    override func collectionView(collectionView: JSQMessagesCollectionView!, avatarImageViewForItemAtIndexPath indexPath: NSIndexPath!) -> UIImageView! {
+//        let message = messages[indexPath.item]
+//
+//    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+   
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        
+    }
+//    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+////        let cell = super.collectionView(collectionView, cellForItemAtIndexPath: indexPath) as! JSQMessagesCollectionViewCell
+////
+////        let message = messages[indexPath.item]
+////        if message.sender() == sender {
+////            cell.textView.textColor = UIColor.blackColor()
+////        } else {
+////            cell.textView.textColor = UIColor.whiteColor()
+////        }
+////
+////        let attributes : [String:AnyObject] = [NSForegroundColorAttributeName:cell.textView.textColor!, NSUnderlineStyleAttributeName: 1]
+////        cell.textView.linkTextAttributes = attributes
+////
+////        //        cell.textView.linkTextAttributes = [NSForegroundColorAttributeName: cell.textView.textColor,
+////        //            NSUnderlineStyleAttributeName: NSUnderlineStyle.StyleSingle]
+////        return cell
+//    }
    
 
 }
--- CareProviderApp/Message.swift
+++ CareProviderApp/Message.swift
@@ -8,8 +8,26 @@
 
 import Foundation
 
-class Message {
+class Message: NSObject {
+    public var message_id: Int!
+    public let name: String
+    public let message: String
+    public var timestamp: Date!
+    public var sender: String!
     
     
+    public init(message_id: Int, name: String, message: String, timestamp: Date, sender: String) {
+        self.message_id = message_id
+        self.name = name
+        self.message = message
+        self.timestamp = timestamp
+        self.sender = sender
+        
+    }
+    
+    public init(name: String, message: String) {
+        self.name = name
+        self.message = message
+    }
     
 }
